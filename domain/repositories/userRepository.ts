8fd8b17992429762c2b7c05697305ffbbef2f35b
H-hiroki-ss <hiroki_hayashi@smartshopping.co.jp>
2021-12-06 19:07:56 +0900

    sampleコードを追加

--- domain/repositories/userRepository.ts
+++ domain/repositories/userRepository.ts
@@ -1,13 +1,41 @@
-import { ApiClient } from "../../infrastructure/api";
+// driverを用いて実際にAPI requestを行う
+// responseをdomainに合わせた形にデータ整形
+import { driver } from "../../infrastructure/driver";
 
 export class userRepository {
-  private readonly _api: ApiClient;
+  private readonly _driver: driver;
 
-  constructor(api: ApiClient) {
-    this._api = api;
+  constructor(api: driver) {
+    this._driver = api;
+  }
+  signin(email, password): Promise<any> {
+    return this._driver
+      .get("/signin", { email, password })
+      .then(() => {
+        // 成功処理
+      })
+      .catch((e) => {
+        console.error(e);
+      });
   }
 
-  signin(email, password) {
-    return this._api.get("/signin", { email, password });
+  async getUserList(email, password): Promise<Object> {
+    const user = await this._driver
+      .get("/signin", { email, password })
+      .then(() => {
+        return ["hoge", "fuga", "piyo"];
+      })
+      .catch((e) => {
+        console.error(e);
+      });
+
+    // 取得したresponseをデータ整形
+    if (user) {
+      return user.map((str) => {
+        return {
+          name: str,
+        };
+      });
+    }
   }
 }
